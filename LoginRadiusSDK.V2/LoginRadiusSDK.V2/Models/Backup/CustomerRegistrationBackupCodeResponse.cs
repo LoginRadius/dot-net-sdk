9529a9de330713ced5c2c85569e55f97f481ad48
Govind Malviya <govindmalviya1987@gmail.com>
2017-08-31 19:22:17 +0530

    Removed Backup from gitignore

---
+++ LoginRadiusSDK.V2/LoginRadiusSDK.V2/Models/Backup/CustomerRegistrationBackupCodeResponse.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace LoginRadiusSDK.V2.Models.Backup
+{
+    public class CustomerRegistrationBackupCodeResponse
+    {
+        public List<string> BackUpCodes { get; set; }
+    }
+}
